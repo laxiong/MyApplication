498291fa8e2249350c0705da172102500a3798d7
xiejin <xiejin@1tbank.com>
2016-05-10 10:00:58 +0800

    webview 标题

--- yitouhang/src/main/java/com/laxiong/Activity/WebViewActivity.java
+++ yitouhang/src/main/java/com/laxiong/Activity/WebViewActivity.java
@@ -3,6 +3,7 @@ package com.laxiong.Activity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -13,36 +14,44 @@ import android.widget.TextView;
 
 import com.laxiong.yitouhang.R;
 
-public class WebViewActivity extends BaseActivity implements View.OnClickListener{
-	/****
-	 * html 页面  协议 活动 等
-	 */
-	private WebView mWebView ;
-	private String urls;
-	private String titleStr ;
-	private FrameLayout mBack ;
-	private TextView mTitle ;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_webview);
-		initData();
-		setlisten();
-	}
+public class WebViewActivity extends BaseActivity implements View.OnClickListener {
+    /****
+     * html 页面  协议 活动 等
+     */
+    private WebView mWebView;
+    private String urls;
+    private String titleStr;
+    private FrameLayout mBack;
+    private TextView mTitle;
 
-	@SuppressLint({ "NewApi", "SetJavaScriptEnabled" }) 
-	private void initData() {
-		mWebView = (WebView)findViewById(R.id.webviews);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        initData();
+        setlisten();
+    }
 
-		mBack = (FrameLayout)findViewById(R.id.back_layout);
-		mTitle =(TextView)findViewById(R.id.title);
+    @SuppressLint({"NewApi", "SetJavaScriptEnabled"})
+    private void initData() {
+        mWebView = (WebView) findViewById(R.id.webviews);
 
-		urls = getIntent().getStringExtra("url");
-		titleStr = getIntent().getStringExtra("title");
+        mBack = (FrameLayout) findViewById(R.id.back_layout);
+        mTitle = (TextView) findViewById(R.id.title);
 
-		if(titleStr!=null)
-			mTitle.setText(titleStr);
+        urls = getIntent().getStringExtra("url");
+        titleStr = getIntent().getStringExtra("title");
+        WebChromeClient wvcc = new WebChromeClient() {
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                mTitle.setText(title);
+            }
 
+        };
+        mWebView.setWebChromeClient(wvcc);
+        if (titleStr != null)
+            mTitle.setText(titleStr);
 //		mWebView.loadUrl(url); //WebView加载web资源
 //		//覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
 //		mWebView.setWebViewClient(new WebViewClient(){
@@ -53,45 +62,45 @@ public class WebViewActivity extends BaseActivity implements View.OnClickListene
 //				return true;
 //			}
 //		});
-		
-		WebSettings settings = mWebView.getSettings();
-		settings.setUseWideViewPort(true);
-		settings.setLoadWithOverviewMode(true);
-		settings.setJavaScriptEnabled(true);
-		settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-		settings.setDomStorageEnabled(true);
-		/*
+
+        WebSettings settings = mWebView.getSettings();
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        settings.setDomStorageEnabled(true);
+        /*
 		 * 4.0-4.2 防范攻击者植入js
 		 */
-		mWebView.removeJavascriptInterface("searchBoxJavaBredge_");
-		mWebView.setWebChromeClient(new WebChromeClient() {
-		});
-		mWebView.setWebViewClient(new WebViewClient(){
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				if(url.startsWith("gongshi1://")){
-					startActivity(new Intent(WebViewActivity.this,
-							GuXiBaoActivity.class));
-					WebViewActivity.this.finish();
-				}
-				mWebView.loadUrl(url);
-				return true ;
-			}
-		});
-		mWebView.loadUrl(urls);
-		
-	}
+        mWebView.removeJavascriptInterface("searchBoxJavaBredge_");
+        mWebView.setWebChromeClient(new WebChromeClient() {
+        });
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("gongshi1://")) {
+                    startActivity(new Intent(WebViewActivity.this,
+                            GuXiBaoActivity.class));
+                    WebViewActivity.this.finish();
+                }
+                mWebView.loadUrl(url);
+                return true;
+            }
+        });
+        mWebView.loadUrl(urls);
 
-	private void setlisten(){
-		mBack.setOnClickListener(this);
-	}
+    }
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()){
-			case R.id.back_layout:
-				this.finish();
-				break;
-		}
-	}
+    private void setlisten() {
+        mBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.back_layout:
+                this.finish();
+                break;
+        }
+    }
 }
